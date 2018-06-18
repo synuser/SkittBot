2df4a266edb6575df938cf200f7e1c0b8a75a46b
Syn <38806740+synuser@users.noreply.github.com>
2018-06-18 21:44:09 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,13 +19,11 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! I'm a group manager bot maintained by [this wonderful person](tg://user?id={}).
-I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick \
-[here](github.com/skittles9823/tgbot)! and the original source [here](github.com/PaulSonOfLars/tgbot)!
+
 
 You can find the list of available commands with /help.
 
 If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
-"""
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
